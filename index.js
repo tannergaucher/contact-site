8640b16016e4f2a96fbbd92d3a5d07630edbcab6
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-04-27 19:58:13 +0200

    fix style

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const video = document.querySelector("video");
 const main = document.querySelector("main");
 
-video.addEventListener("play", function () {
-  main.style.display = "none";
-});
+main.style.display = "none";
 
 video.addEventListener("ended", function () {
   video.style.display = "none";
