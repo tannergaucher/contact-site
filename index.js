cb33123a9c5d09f76069ff288872f7b9048d9a27
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-04-28 15:25:39 +0200

    Add skip button

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const video = document.querySelector("video");
 const main = document.querySelector("main");
+const skipButton = document.querySelector(".skip-button");
 
 main.style.display = "none";
+skipButton.style.display = "none";
+
+setTimeout(() => {
+  skipButton.style.display = "block";
+}, 5000);
+
+skipButton.addEventListener("click", function () {
+  video.style.display = "none";
+  skipButton.style.display = "none";
+  main.style.display = "block";
+});
 
 video.addEventListener("ended", function () {
   video.style.display = "none";
+  skipButton.style.display = "none";
   main.style.display = "block";
 });
