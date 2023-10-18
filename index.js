432ee5e3684fba66306d3f456f1ace84d58873dd
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-10-18 16:24:26 -0400

    updates

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const video = document.querySelector("video");
+const header = document.querySelector("header");
 const main = document.querySelector("main");
 const skipButton = document.querySelector(".skip-button");
 
+header.style.display = "none";
 main.style.display = "none";
 skipButton.style.display = "none";
 
 setTimeout(() => {
   skipButton.style.display = "block";
-}, 1500);
+}, 700);
 
 skipButton.addEventListener("click", function () {
   video.style.display = "none";
   skipButton.style.display = "none";
+  header.style.display = "block";
   main.style.display = "block";
 });
 
 video.addEventListener("ended", function () {
   video.style.display = "none";
   skipButton.style.display = "none";
+  header.style.display = "block";
   main.style.display = "block";
 });
