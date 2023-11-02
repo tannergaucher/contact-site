5616123020e85d21bf5fec848e9110b6bbc2d186
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2023-11-02 17:05:37 -0400

    update design

---
+++ avatar-quote-dialog/index.ts
@@ -0,0 +1,83 @@
+const quotes = [
+  "👋  Hey, I'm Tanner.",
+  "😄  Nice to meet you!",
+  "🖥️  I like building things for the web.",
+  "undefined",
+  "🐛  Hah, i bet you thought that was a bug!",
+  '🤓  I just wrote "undefined" there as a joke.',
+  "😅  Ha ha.",
+  "😎  Anyways, let's work together. Hire me!",
+];
+
+const avatarQuoteContainer = document.querySelector(".avatar-quote-container");
+
+if (!avatarQuoteContainer) {
+  throw new Error("Could not find .avatar-quote-container");
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      restartQuotes();
+    }
+  });
+});
+
+observer.observe(avatarQuoteContainer);
+
+function setQuotes(quotes: string[]) {
+  const avatarQuoteContainer = document.querySelector(
+    ".avatar-quote-container"
+  );
+
+  if (!avatarQuoteContainer) {
+    throw new Error("Could not find .avatar-quote-container");
+  }
+
+  const avatarQuotes = avatarQuoteContainer.querySelectorAll(".avatar-quote");
+
+  const currentQuote = avatarQuotes[avatarQuotes.length - 1];
+  let nextQuoteIndex = quotes.indexOf(currentQuote.innerHTML) + 1;
+  const nextQuote = document.createElement("blockquote");
+
+  if (nextQuoteIndex >= quotes.length) {
+    const allQuotes = document.querySelectorAll(".avatar-quote");
+    allQuotes.forEach((quote) => quote.remove());
+    nextQuoteIndex = 0;
+  }
+
+  nextQuote.classList.add("avatar-quote");
+  nextQuote.setAttribute("avatar-quote-number", `${nextQuoteIndex + 1}`);
+
+  const allQuotes = document.querySelectorAll(".avatar-quote");
+  allQuotes.forEach((quote) => quote.removeAttribute("current-quote"));
+
+  nextQuote.setAttribute("current-quote", "true");
+  nextQuote.innerText = quotes[nextQuoteIndex];
+  avatarQuoteContainer?.appendChild(nextQuote);
+}
+
+let interval: number | null = null;
+
+function restartQuotes() {
+  if (interval) {
+    clearInterval(interval);
+  }
+
+  const existingQuotes = document.querySelectorAll(".avatar-quote");
+  existingQuotes.forEach((quote) => quote.remove());
+
+  const initialQuote = document.createElement("blockquote");
+  initialQuote.classList.add("avatar-quote");
+  initialQuote.setAttribute("avatar-quote-number", "1");
+  initialQuote.setAttribute("current-quote", "true");
+  initialQuote.innerText = quotes[0];
+  avatarQuoteContainer?.appendChild(initialQuote);
+
+  interval = setInterval(() => {
+    setQuotes(quotes);
+  }, 1900);
+}
+
+// Initial start
+restartQuotes();
